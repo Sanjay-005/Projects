0228da4ab875a46429dbe7b6299eac9ef37935e2
Sanjay-005 <sanjayshakthivel005@gmail.com>
2025-08-09 21:45:37 +0530

    Update ProductDetails.jsx

--- ecommerce-app/client/src/ProductDetails.jsx
+++ ecommerce-app/client/src/ProductDetails.jsx
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
 
 function ProductDetails({products, addToCart}) {
-    const { id } = useParams();//useParams() returns object and variable id stores the id from the returned object!
-    const product = products.find(p => p.id === parseInt(id));//returns returns the first product where the p.id equals the parsed id, if none match it will be undefined!!
+    const { id } = useParams();//useParams() returns object and variable id stores the id as a string from the returned object!
+    const product = products.find(p => p.id === parseInt(id));//returns the first product where the p.id equals the parsed id, if none match it will be undefined!!
     if(!product) return <h2>Product not found</h2>;
 
     return (
