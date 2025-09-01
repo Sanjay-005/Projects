98c0d03e2ebc8422adf99bc2ebb4ee942abdac01
Sanjay-005 <sanjayshakthivel005@gmail.com>
2025-09-01 21:13:31 +0530

    Update

--- ecommerce-app/server/models/Product.js
+++ ecommerce-app/server/models/Product.js
@@ -1,13 +1,3 @@
-// import mongoose from "mongoose";
-
-// const productSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   price: { type: Number, required: true, min: 0 },
-//   image: { type: String },
-// });
-
-// export default mongoose.model("Product", productSchema);
-
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
