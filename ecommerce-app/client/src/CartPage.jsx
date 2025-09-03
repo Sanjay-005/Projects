c578f807c8013c2a5db9825cc0c27d49f1392613
Sanjay-005 <sanjayshakthivel005@gmail.com>
2025-09-03 23:00:50 +0530

    Update files

--- ecommerce-app/client/src/CartPage.jsx
+++ ecommerce-app/client/src/CartPage.jsx
@@ -1,31 +1,3 @@
-// // src/CartPage.jsx
-// function CartPage({ cart, removeFromCart }) {
-//   return (
-//     <div>
-//       <h2>Your Cart</h2>
-//       {cart.length === 0 ? (
-//         <p>Your cart is empty.</p>
-//       ) : (
-//         <ul style={{ listStyle: "none", padding: 0 }}>
-//           {cart.map((item, index) => (
-//             <li key={index} style={{ borderBottom: "1px solid #ddd", padding: "8px 0" }}>
-//               {item.name} - ₹{item.price}
-//               <button
-//                 style={{ marginLeft: 8 }}
-//                 onClick={() => removeFromCart(index)}
-//               >
-//                 Remove
-//               </button>
-//             </li>
-//           ))}
-//         </ul>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default CartPage;
-
 import { useNavigate } from "react-router-dom";
 
 function CartPage({ cart, removeFromCart }) {
