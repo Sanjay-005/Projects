65201919b4eaa4b4a710c489ee35f68e5aff2fab
Sanjay-005 <sanjayshakthivel005@gmail.com>
2025-12-25 19:20:52 +0530

    ok

--- ecommerce-app/server/index.js
+++ ecommerce-app/server/index.js
@@ -13,7 +13,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ecommerce-rho-peach.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/api/search", searchRoutes);/** */
 app.use("/api/payment", paymentRoutes);
